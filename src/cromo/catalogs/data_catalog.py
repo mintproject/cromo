3e56a60fae6c824a599135aef7723f89ba58e98e
Varun Ratnakar <varunratnakar@gmail.com>
2021-03-28 15:03:02 +0530

    Fixes for Data Catalog query

--- src/cromo/catalogs/data_catalog.py
+++ src/cromo/catalogs/data_catalog.py
@@ -3,22 +3,48 @@ import json
 
 from cromo.constants import DATA_CATALOG_URL
 
-# Get all datasets having the given variables, and relevant for spatio-temporal information
+# Get all datasets having the given variables that are relevant for the provided spatio-temporal information
 def getMatchingDatasets(variables, geojson_file, start_date, end_date):
-    with open(geojson_file) as fd:
+     with open(geojson_file) as fd:
         geojson = json.load(fd)
+        data = {
+            "standard_variable_names__in": variables,
+            "spatial_coverage__intersects": geojson["geometry"],
+            "start_time__lte": dateTimeToXSD(start_date),
+            "end_time__gte": dateTimeToXSD(end_date),
+            "limit": 100
+        }
         response = requests.post(
             url= DATA_CATALOG_URL + "/datasets/find",
-            json= {
-                "standard_variable_names__in": variables,
+            json= data
+        )
+        response = response.json()
+        if "result" in response and response["result"] == "success":
+            return response["datasets"]
+        return []
+
+# Get all resources for a dataset that are relevant for the provided spatio-temporal information
+def getMatchingDatasetResources(dsid, geojson_file, start_date, end_date):
+    with open(geojson_file) as fd:
+        geojson = json.load(fd)
+        data = {
+            "dataset_id": dsid,
+            "filter": {
                 "spatial_coverage__intersects": geojson["geometry"],
                 "start_time__lte": dateTimeToXSD(start_date),
-                "end_time__gte": dateTimeToXSD(end_date),
-                "limit": 100
-            }
+                "end_time__gte": dateTimeToXSD(end_date)
+            },
+            "limit": 5000            
+        }
+        response = requests.post(
+            url= DATA_CATALOG_URL + "/datasets/dataset_resources",
+            json= data
         )
-        datasets = response.json()
-        return datasets
+        response = response.json()
+        if "result" in response and response["result"] == "success":
+            return response["dataset"]["resources"]
+        return []
+
 
 def dateTimeToXSD(dt):
     return str(dt).replace(" ", "T")
--- src/cromo/catalogs/model_catalog.py
+++ src/cromo/catalogs/model_catalog.py
@@ -1,10 +1,8 @@
 
-from json.decoder import JSONDecoder
 from modelcatalog.api.dataset_specification_api import DatasetSpecificationApi
-import requests
-import json
 from modelcatalog import api_client, configuration
 from modelcatalog.api.model_configuration_api import ModelConfigurationApi
+from modelcatalog.api.model_configuration_setup_api import ModelConfigurationSetupApi
 from modelcatalog.api.variable_presentation_api import VariablePresentationApi
 from modelcatalog.api.dataset_specification_api import DatasetSpecificationApi
 from modelcatalog.exceptions import ApiException
@@ -12,7 +10,7 @@ from cromo.constants import MODEL_CATALOG_URL, DEFAULT_USERNAME, getLocalName
 
 MC_API_CLIENT=api_client.ApiClient(configuration.Configuration(host=MODEL_CATALOG_URL))
 
-# Get all model configurationsr
+# Get all model configurations
 def getAllModelConfigurations():
     try:
         # List all Model entities
@@ -43,24 +41,16 @@ def getModelConfigurationDetails(config):
             config.has_input = new_inputs
         return config
     except ApiException as e:
-        print("Exception when calling ModelConfigurationApi->custom_modelconfigurations_id_get({}): {}\n".format(id, e))
+        print("Exception when getting model configuration details({}): {}\n".format(config.id, e))
     return None
 
-# create an instance of the API class
-def getModelConfigurationDetails2(id):
+# Get all model configuration setups
+def getAllModelConfigurationSetups():
     try:
         # List all Model entities
-        api_instance = ModelConfigurationApi(api_client=MC_API_CLIENT)
-        dspec_api = DatasetSpecificationApi(api_client=MC_API_CLIENT)
-        config = api_instance.custom_modelconfigurations_id_get(getLocalName(id), username=DEFAULT_USERNAME)
-        if config.has_input is not None:
-            new_inputs = []
-            for input in config.has_input:
-                new_input = dspec_api.datasetspecifications_id_get(getLocalName(input.id), username=DEFAULT_USERNAME)
-                new_inputs.append(new_input)
-            print(new_inputs)
-            config.has_input = new_inputs
-        return config
+        api_instance = ModelConfigurationSetupApi(api_client=MC_API_CLIENT)
+        configs = api_instance.modelconfigurationsetups_get(username=DEFAULT_USERNAME)
+        return configs
     except ApiException as e:
-        print("Exception when calling ModelConfigurationApi->custom_modelconfigurations_id_get({}): {}\n".format(id, e))
-    return None
+        print("Exception when calling ModelConfigurationApi->modelconfigurations_get: %s\n" % e)
+    return []
