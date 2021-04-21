333988f1177f96f09cccc4e9ef83cb0ae0fea715
Varun Ratnakar <varunratnakar@gmail.com>
2021-04-21 13:53:13 +0530

    Added API to search models for a scenario

--- src/cromo/api.py
+++ src/cromo/api.py
@@ -1,9 +1,24 @@
-from cromo.catalogs.model_catalog import checkConfigViability
+from cromo.catalogs.model_catalog import checkConfigViability, getAllModelConfigurations
 from cromo._utils import parse_datetime
+import re
 
-def checkConfigValidityAPI(configId, body):
+def checkModelConfigurationValidity(configId, body):
     return checkConfigViability(
         configId, 
         body["spatial"], 
         parse_datetime(body["temporal"]["start_date"]),
         parse_datetime(body["temporal"]["end_date"]))
+
+
+def fetchModelConfigurationsForScenario(scenario):
+    # Get all matching model configurations (or setups ?)
+    configs = getAllModelConfigurations()
+    matching_configs = []
+    for config in configs:
+        if config.description:
+            if (re.search(scenario, config.description[0], re.IGNORECASE) or 
+                re.search(scenario, config.label[0], re.IGNORECASE)):
+                matching_configs.append({
+                    "id": config.id,
+                    "name": config.label[0]})
+    return matching_configs
