2d546039a80ffb3650ef207c4ed0df28d4200372
Varun Ratnakar <varunratnakar@gmail.com>
2021-04-22 22:12:03 +0530

    Updated with cors

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import connexion
+from flask_cors import CORS
 
 if __name__ == '__main__':
-    app = connexion.FlaskApp(__name__, host="localhost", port=8080, specification_dir='openapi/')
+    app = connexion.FlaskApp(__name__, host="localhost", port=9090, specification_dir='openapi/')
     app.add_api('cromo-api.yaml')
+    CORS(app.app)
     app.run()
