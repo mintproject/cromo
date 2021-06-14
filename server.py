773a6c1f0c613df4d570b807b22100c66bfbbd2c
Maximiliano Osorio <maxiosorio@gmail.com>
2021-06-14 17:19:02 -0400

    fix: listen all interfaces

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import connexion
 from flask_cors import CORS
 
 if __name__ == '__main__':
-    app = connexion.FlaskApp(__name__, host="localhost", port=9090, specification_dir='openapi/')
+    app = connexion.FlaskApp(__name__, host="0.0.0.0", port=9090, specification_dir='openapi/')
     app.add_api('cromo-api.yaml')
     CORS(app.app)
     app.run()
