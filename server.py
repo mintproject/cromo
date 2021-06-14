cf52218f152d73543c0df7ee315f01681c16f764
Maximiliano Osorio <maxiosorio@gmail.com>
2021-06-14 17:25:23 -0400

    fix: avoid redirect

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import connexion
 from flask_cors import CORS
 
 if __name__ == '__main__':
-    app = connexion.FlaskApp(__name__, host="0.0.0.0", port=9090, specification_dir='openapi/')
+    app = connexion.FlaskApp(__name__, specification_dir='./openapi/')
     app.add_api('cromo-api.yaml')
     CORS(app.app)
-    app.run()
+    app.run(port=9090)
